9d1c1e4d614887d3f40d485421faa2f3141a071e
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2015-01-02 17:40:58 -0500

    Working version of the GLUT window
    This commit works with VTK commit 4190402

--- main.cpp
+++ main.cpp
@@ -8,19 +8,31 @@
 #include <vtkCamera.h>
 #include <ExternalVTKWidget.h>
 #include <vtkExternalOpenGLRenderer.h>
-#include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkExternalOpenGLRenderWindow.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkSphereSource.h>
 #include <vtkOBJReader.h>
+#include <vtkCallbackCommand.h>
+#include <vtkCommand.h>
 
 #include <vtkNew.h>
 
 vtkNew<ExternalVTKWidget> externalVTKWidget;
+vtkNew<vtkExternalOpenGLRenderWindow> renWin;
 //vtkNew<vtkOBJReader> objReader;
 
 bool initilaized = false;
+static int windowId = -1;
+
+static void MakeCurrentCallback(vtkObject* caller,
+                                long unsigned int eventId,
+                                void * clientData,
+                                void * callData)
+{
+  glutSetWindow(windowId);
+}
 
 /* Handler for window-repaint event. Call back when the window first appears and
    whenever the window needs to be re-painted. */
@@ -32,20 +44,35 @@ void display()
      vtkNew<vtkActor> actor;
      actor->SetMapper(mapper.GetPointer());
      externalVTKWidget->GetRenderer()->AddActor(actor.GetPointer());
+     renWin->SetSize(400,400);
+//     renWin->SetPosition(400,200);
+//     vtkNew<vtkCallbackCommand> callback;
+//     callback->SetCallback(MakeCurrentCallback);
+//     renWin->AddObserver(vtkCommand::WindowMakeCurrentEvent, callback.GetPointer());
+     externalVTKWidget->SetRenderWindow(renWin.GetPointer());
      vtkNew<vtkSphereSource> ss;
+     ss->SetCenter(-0.5,0.5,0);
      mapper->SetInputConnection(ss->GetOutputPort());
 //     mapper->SetInputConnection(objReader->GetOutputPort());
      externalVTKWidget->GetRenderer()->ResetCamera();
      initilaized = true;
      }
 
-   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+   glClearColor(0.0f, 0.0f, 0.0f, 0.5f); // Set background color to black and opaque
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the color buffer
 
    glFlush();  // Render now
+   glBegin(GL_TRIANGLES);
+     glVertex3f(-0.5,-0.5,0.0);
+     glVertex3f(0.5,-0.5,0.0);
+     glVertex3f(0.5,0.5,0.0);
+   glEnd();
 
-   externalVTKWidget->GetRenderWindow()->SetSize(400, 400);
+//  externalVTKWidget->GetRenderWindow()->SetSize(400, 400);
    externalVTKWidget->GetRenderWindow()->Render();
+
+  /* Display the result */
+  glutSwapBuffers();
 }
 
 /* Main function: GLUT runs as a console application starting at main()  */
@@ -53,8 +80,8 @@ int main(int argc, char** argv) {
 //   objReader->SetFileName(argv[1]);
    glutInit(&argc, argv);                 // Initialize GLUT
    glutInitWindowSize(400,400);   // Set the window's initial width & height
-   glutInitWindowPosition(0, 0); // Position the window's initial top-left corner
-   glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+   glutInitWindowPosition(400, 200); // Position the window's initial top-left corner
+   windowId = glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
    glutDisplayFunc(display); // Register display callback handler for window re-paint
    glutMainLoop();           // Enter the infinitely event-processing loop
    return 0;
