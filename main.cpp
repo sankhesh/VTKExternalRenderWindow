9f6866227e33280bbb17ba8299bcedd4846b53ad
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2015-01-08 10:27:09 -0500

    WIP: Better GLUT test

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkPolyDataMapper.h>
-#include <vtkSphereSource.h>
+#include <vtkCubeSource.h>
 #include <vtkOBJReader.h>
 #include <vtkCallbackCommand.h>
 #include <vtkCommand.h>
@@ -50,44 +50,56 @@ void display()
      actor->SetMapper(mapper.GetPointer());
      externalVTKWidget->GetRenderer()->AddActor(actor.GetPointer());
      renWin->SetSize(400,400);
-//     renWin->SetPosition(400,200);
-//     vtkNew<vtkCallbackCommand> callback;
-//     callback->SetCallback(MakeCurrentCallback);
-//     renWin->AddObserver(vtkCommand::WindowMakeCurrentEvent, callback.GetPointer());
+     vtkNew<vtkCallbackCommand> callback;
+     callback->SetCallback(MakeCurrentCallback);
+     renWin->AddObserver(vtkCommand::WindowMakeCurrentEvent,
+                         callback.GetPointer());
      externalVTKWidget->SetRenderWindow(renWin.GetPointer());
-     vtkNew<vtkSphereSource> ss;
+     vtkNew<vtkCubeSource> ss;
      ss->SetCenter(-0.5,0.5,0);
      mapper->SetInputConnection(ss->GetOutputPort());
-//     mapper->SetInputConnection(objReader->GetOutputPort());
      externalVTKWidget->GetRenderer()->ResetCamera();
+     externalVTKWidget->GetRenderer()->PreserveColorBufferOff();
+     //externalVTKWidget->GetRenderer()->PreserveDepthBufferOff();
+     actor->RotateX(45.0);
      initilaized = true;
      }
 
-   glClearColor(0.0f, 0.0f, 0.0f, 0.5f); // Set background color to black and opaque
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+   glClearDepth(1.0f);
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the color buffer
 
    glFlush();  // Render now
-   glBegin(GL_TRIANGLES);
-     glVertex3f(-0.5,-0.5,0.0);
-     glVertex3f(0.5,-0.5,0.0);
-     glVertex3f(0.5,0.5,0.0);
-   glEnd();
+//   glBegin(GL_TRIANGLES);
+//     glVertex3f(-0.5,-0.5,0.0);
+//     glVertex3f(0.5,-0.5,0.0);
+//     glVertex3f(0.5,0.5,0.0);
+//   glEnd();
 
-//  externalVTKWidget->GetRenderWindow()->SetSize(400, 400);
    externalVTKWidget->GetRenderWindow()->Render();
 
   /* Display the result */
   glutSwapBuffers();
 }
 
+void handleResize(int w, int h)
+{
+  externalVTKWidget->GetRenderWindow()->SetSize(w, h);
+}
+
 /* Main function: GLUT runs as a console application starting at main()  */
 int main(int argc, char** argv) {
 //   objReader->SetFileName(argv[1]);
    glutInit(&argc, argv);                 // Initialize GLUT
+   glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
    glutInitWindowSize(400,400);   // Set the window's initial width & height
    glutInitWindowPosition(400, 200); // Position the window's initial top-left corner
    windowId = glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
    glutDisplayFunc(display); // Register display callback handler for window re-paint
+   glutReshapeFunc(handleResize); // Register resize callback handler for window resize
+   glEnable(GL_DEPTH_TEST);
+   glDepthMask(GL_TRUE);
+   glDepthFunc(GL_LEQUAL);
    glutMainLoop();           // Enter the infinitely event-processing loop
    return 0;
 }
